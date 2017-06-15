58559534b4b6956414db015bbc851b2a49a32766
Tony <saric.tony@gmail.com>
2017-06-15 14:52:40 -0700

    adds adds user to table

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170615180150) do
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
