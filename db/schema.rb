aa85ab26d438044fd251c8c5a875252f3575f1aa
Tony <saric.tony@gmail.com>
2017-06-15 16:31:26 -0700

    adds completed_at ability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615180150) do
+ActiveRecord::Schema.define(version: 20170615231848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20170615180150) do
     t.string   "title"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
 
